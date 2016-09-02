99aa9e03cfcc785966f981aa4694be99beafb452
aacohen <arielaades@gmail.com>
2016-09-01 20:35:07 -0400

    Done.

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -18,6 +18,28 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     return YES;
 }
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array
+{ NSMutableArray *firstArray = [array mutableCopy];
+    [firstArray addObject:string];
+    return firstArray;}
+ -(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array
+{NSUInteger numberOfUppercaseStrings =0;
+    
+    for (NSUInteger i = 0; i < [array count]; i++)
+    
+    { NSString *upperCaseVersion = [[array objectAtIndex:(i)] uppercaseString];
+        
+        if ([upperCaseVersion isEqualToString: [array objectAtIndex:(i)]])
+        {numberOfUppercaseStrings = numberOfUppercaseStrings + 1;}
+    }
+    return numberOfUppercaseStrings;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array
+
+{ [array removeAllObjects];
+
+}
 
 /*
  
